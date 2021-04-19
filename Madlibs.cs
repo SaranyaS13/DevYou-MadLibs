3d6e0601e9ab0d36948d638f5703348b307b2ca6
SaranyaS13 <82345946+SaranyaS13@users.noreply.github.com>
2021-04-18 22:27:13 -0400

    Added Comment

--- Madlibs.cs
+++ Madlibs.cs
@@ -6,6 +6,7 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
+            /*Declaration of Variables and getting user input */
             Console.WriteLine("Enter any adjective...");
             String adjective1 = Console.ReadLine();
             Console.WriteLine("Enter any noun...");
@@ -30,6 +31,7 @@ namespace MadLibs
             String pastverb2 = Console.ReadLine();
             Console.WriteLine("Enter any adjective...");
             String adjective4 = Console.ReadLine();
+            /* Priniting to Console */
             Console.WriteLine("Today I went to the zoo. I saw a {0} {1} jumping up and down in its tree. He {2} {3} through \n" +
                 "the large tunnel that led to its {4} {5} . I got some peanuts and passed them through the cage to a gigantic gray \n" +
                 "{6} towering above my head.Feeding that animal made me hungry.I went to get a {7} scoop of ice \n"+               
